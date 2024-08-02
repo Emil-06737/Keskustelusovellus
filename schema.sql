33a2cfc3733918a4d22780d31ce863dc00af3642
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2024-08-02 15:46:33 +0300

    Add schema.sql

---
+++ schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE discussion_areas (
+    id SERIAL PRIMARY KEY,
+    topic TEXT,
+    confidential BOOLEAN
+);
+
+CREATE TABLE discussion_chains (
+    id SERIAL PRIMARY KEY,
+    header TEXT,
+    discussion_area_id INTEGER REFERENCES discussion_areas ON DELETE CASCADE,
+    creator_id INTEGER REFERENCES users
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    discussion_chain_id INTEGER REFERENCES discussion_chains ON DELETE CASCADE,
+    creator_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);
+
+CREATE TABLE users_of_confidential_discussion_areas (
+    id SERIAL PRIMARY KEY,
+    discussion_area_id INTEGER REFERENCES discussion_areas ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users
+);
