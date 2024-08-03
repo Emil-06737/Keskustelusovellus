d331398b50237c65cf70b1cf6dc88a317898d73c
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2024-08-03 17:06:20 +0300

    Add discussion_areas

---
+++ discussion_areas.py
@@ -0,0 +1,9 @@
+from db import db
+from sqlalchemy.sql import text
+
+def get_stats():
+    sql = """SELECT da.id, da.topic, COUNT(dc.id), COUNT(m.id), MAX(m.sent_at)
+             FROM discussion_areas da LEFT JOIN discussion_chains dc
+             ON da.id = dc.discussion_area_id LEFT JOIN messages m ON dc.id = m.discussion_chain_id
+             GROUP BY da.id ORDER BY da.id"""
+    return db.session.execute(text(sql)).fetchall()
--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+import discussion_areas
 
 @app.route("/")
 def index():
