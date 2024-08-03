e3fd07e89c2ac84c2e71ba27c4c27ee7e3391246
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2024-08-03 20:07:01 +0300

    Create login page

--- routes.py
+++ routes.py
@@ -5,3 +5,7 @@ import discussion_areas
 @app.route("/")
 def index():
     return render_template("index.html", areas=discussion_areas.get_stats())
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
