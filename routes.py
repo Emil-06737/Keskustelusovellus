f5c73e5ceb28b1fb40e5b0cf51e168748a3d0d04
Emil Huuki <116680128+Emil-06737@users.noreply.github.com>
2024-08-04 11:24:43 +0300

    Create registration

--- routes.py
+++ routes.py
@@ -24,3 +24,24 @@ def login():
 def logout():
     users.logout()
     return redirect("/")
+
+@app.route("/register", methods=["get", "post"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        if len(username) < 3 or len(username) > 20:
+            return render_template("error.html", message="Tunnuksessa tulee olla 3-20 merrkiä.")
+        
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat.")
+        if password1 == "":
+            return render_template("error.html", message="Salasana on tyhjä.")
+        
+        if not users.register(username, password1):
+            return render_template("error.html", message="Rekisteröinti ei onnistunut.")
+        return redirect("/")
--- users.py
+++ users.py
@@ -2,7 +2,7 @@ import os
 from db import db
 from flask import session
 from sqlalchemy.sql import text
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(name, password):
     sql = "SELECT id, name, password, admin FROM users WHERE name=:name"
@@ -21,3 +21,13 @@ def logout():
     del session["user_id"]
     del session["user_name"]
     del session["user_admin"]
+
+def register(name, password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (name, password, admin) VALUES (:name, :password, :admin)"
+        db.session.execute(text(sql), {"name":name, "password":hash_value, "admin":False})
+        db.session.commit()
+    except:
+        return False
+    return login(name, password)
